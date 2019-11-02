d4924b475c4a6e289c59381f960e683d638d8f1f
Anthony Caron <antcar1992@gmail.com>
2019-11-02 11:04:09 -0700

    added footer /w timer btn, timer container

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('JS connected');
+// Timer element variables
+const timerToggleBtn = document.querySelector('#timer-toggle-btn');
+const timerBackground = document.querySelector('.timer__background');
+const timerContainer = document.querySelector('.timer__container');
+
+// Timer btn event listener
+timerToggleBtn.addEventListener('click', e => {
+	timerBackground.classList.toggle('timer__background--active');
+	timerContainer.classList.toggle('timer__container--active');
+});
+
+// Timer background event listener
+timerBackground.addEventListener('click', e => {
+	timerBackground.classList.remove('timer__background--active');
+	timerContainer.classList.remove('timer__container--active');
+});
