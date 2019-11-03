ccc3fb3601cb1ac77d94416e40fe5b3990b76f3a
Anthony Caron <antcar1992@gmail.com>
2019-11-02 18:19:42 -0700

    timer toggle btn

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,40 @@
 // Timer element variables
 const timerToggleBtn = document.querySelector('#timer-toggle-btn');
 const timerBackground = document.querySelector('.timer__background');
-const timerContainer = document.querySelector('.timer__container');
+const timerCard = document.querySelector('.timer__card');
+const timerCloseBtn = document.querySelector('.timer__close-btn');
+const timerToggle = document.querySelector('.timer__toggle');
+const timerToggleBlock = document.querySelector('.timer__toggle-block');
+
+timerToggle.addEventListener('click', e => {
+	timerToggleBlock.classList.toggle('timer__toggle-block--right');
+	if (timerToggleBlock.classList.contains('timer__toggle-block--right')) {
+		timerToggleBlock.innerHTML = '<span>Timer</span>';
+	} else {
+		timerToggleBlock.innerHTML = '<span>Stopwatch</span>';
+	}
+});
 
 // Timer btn event listener
 timerToggleBtn.addEventListener('click', e => {
 	timerBackground.classList.toggle('timer__background--active');
-	timerContainer.classList.toggle('timer__container--active');
+	timerCard.classList.toggle('timer__card--active');
+	timerCloseBtn.classList.toggle('timer__close-btn--active');
 });
 
 // Timer background event listener
 timerBackground.addEventListener('click', e => {
-	timerBackground.classList.remove('timer__background--active');
-	timerContainer.classList.remove('timer__container--active');
+	closeTimer(e);
 });
+
+// Timer Close Btn event listener
+timerCloseBtn.addEventListener('click', e => {
+	closeTimer(e);
+});
+
+// Close Timer Function
+function closeTimer(e) {
+	timerBackground.classList.remove('timer__background--active');
+	timerCard.classList.remove('timer__card--active');
+	timerCloseBtn.classList.remove('timer__close-btn--active');
+}
