4aedc76d5e2b3cdd71f963d1498592b73f696668
Anthony Caron <antcar1992@gmail.com>
2019-11-04 23:12:06 -0800

    initial stopwatch code

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,10 @@
+// Timer - Show/Hide
+
 // Timer element variables
 const toggleNavBtn = document.querySelector('#timer-toggle-btn');
 const timerBackground = document.querySelector('.timer__background');
 const timerCard = document.querySelector('.timer__card');
 const timerCloseBtn = document.querySelector('.timer__close-btn');
-const timerToggle = document.querySelector('.timer__toggle');
-const timerToggleBlock = document.querySelector('.timer__toggle-block');
-const timerToggleLeft = document.querySelector('#timer__toggle--left');
-const timerToggleRight = document.querySelector('#timer__toggle--right');
-
-timerToggle.addEventListener('click', e => {
-	timerToggleBlock.classList.toggle('timer__toggle-block--right');
-	timerToggleLeft.classList.toggle('timer__toggle-option--active');
-	timerToggleRight.classList.toggle('timer__toggle-option--active');
-});
 
 // Timer btn event listener
 toggleNavBtn.addEventListener('click', e => {
@@ -36,4 +28,46 @@ function closeTimer(e) {
 	timerBackground.classList.remove('timer__background--active');
 	timerCard.classList.remove('timer__card--active');
 	timerCloseBtn.classList.remove('timer__close-btn--active');
+}
+
+// Stopwatch/Timer Toggle Block
+const timerToggle = document.querySelector('.timer__toggle');
+const timerToggleBlock = document.querySelector('.timer__toggle-block');
+const timerToggleLeft = document.querySelector('#timer__toggle--left');
+const timerToggleRight = document.querySelector('#timer__toggle--right');
+
+timerToggle.addEventListener('click', e => {
+	timerToggleBlock.classList.toggle('timer__toggle-block--right');
+	timerToggleLeft.classList.toggle('timer__toggle-option--active');
+	timerToggleRight.classList.toggle('timer__toggle-option--active');
+});
+
+// Stopwatch Btn Vars
+import { stopwatchActive, stopwatchStart, stopwatchStop } from './stopwatch.js';
+
+const stopwatchPlayBtn = document.querySelector('#stopwatch_play_btn');
+const stopwatchPauseBtn = document.querySelector('#stopwatch_pause_btn');
+const stopwatchResetBtn = document.querySelector('#stopwatch_reset_btn');
+
+changeStopwatchButtons();
+
+stopwatchPlayBtn.addEventListener('click', e => {
+	stopwatchStart(e);
+	changeStopwatchButtons();
+});
+
+stopwatchPauseBtn.addEventListener('click', e => {
+	stopwatchStop(e);
+	changeStopwatchButtons();
+});
+
+// Checks if stopwatch is active, hides play btn and shows pause btn
+function changeStopwatchButtons() {
+	if (stopwatchActive) {
+		stopwatchPlayBtn.classList.add('hidden');
+		stopwatchPauseBtn.classList.remove('hidden');
+	} else {
+		stopwatchPlayBtn.classList.remove('hidden');
+		stopwatchPauseBtn.classList.add('hidden');
+	}
 }
---
+++ public/scripts/stopwatch.js
@@ -0,0 +1,59 @@
+const timerText = document.querySelector('.timer__text');
+
+export let stopwatchActive = false;
+export const stopwatchTime = {
+	hours   : 0,
+	minutes : 0,
+	seconds : 0
+};
+let interval;
+
+export function stopwatchStart(e) {
+	stopwatchActive = true;
+	interval = setInterval(() => {
+		increaseSecs();
+	}, 1000);
+}
+
+export function stopwatchStop(e) {
+	stopwatchActive = false;
+	clearInterval(interval);
+}
+
+function increaseSecs() {
+	if (stopwatchTime.seconds < 60) {
+		stopwatchTime.seconds++;
+	} else {
+		stopwatchTime.seconds = 0;
+		if (stopwatchTime.minutes < 60) {
+			stopwatchTime.minutes++;
+		} else {
+			stopwatchTime.minutes = 0;
+			stopwatchTime.hours++;
+		}
+	}
+
+	timerText.innerText = updateHTML();
+}
+
+function updateHTML() {
+	let hrs;
+	let mins;
+	let secs;
+	if (stopwatchTime.hours < 10) {
+		hrs = '0' + stopwatchTime.hours;
+	} else {
+		hrs = '' + stopwatchTime.hours;
+	}
+	if (stopwatchTime.minutes < 10) {
+		mins = '0' + stopwatchTime.minutes;
+	} else {
+		mins = '' + stopwatchTime.minutes;
+	}
+	if (stopwatchTime.seconds < 10) {
+		secs = '0' + stopwatchTime.seconds;
+	} else {
+		secs = '' + stopwatchTime.seconds;
+	}
+	return `${hrs}:${mins}:${secs}`;
+}
