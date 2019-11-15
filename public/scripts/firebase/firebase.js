e261cca5e799e439080192c9ecff2d0f99da3929
Anthony Caron <antcar1992@gmail.com>
2019-11-15 15:30:40 -0800

    fixed git error in firebase

--- public/scripts/firebase/firebase.js
+++ public/scripts/firebase/firebase.js
@@ -54,163 +54,95 @@ function createUser() {
 /** Adds a Course **/
 
 export const writeDB = {
+	// Retrieves the course name and color selected by the user
+	addCourse : function() {
+		// for an <input type='text'> with id = 'courseName'
+		var courseName = document.getElementById('courseName');
+		// for an <input type='color'> with id = 'courseColor'
+		var courseColor = document.getElementById('courseColor');
+		var courseDate = new Date();
 
-<<<<<<< HEAD
-// Retrieves the course name and color selected by the user
-function addCourse() {
-=======
- // Retrieves the course name and color selected by the user
- addCourse: function() {
-
->>>>>>> 689c9d59ca222d191f724549b93dc6cba5c28f47
-	// for an <input type='text'> with id = 'courseName'
-	var courseName = document.getElementById('courseName');
-	// for an <input type='color'> with id = 'courseColor'
-	var courseColor = document.getElementById('courseColor');
-	var courseDate = new Date();
-
-	// for a <form> with an id = 'courseForm'
-	// if no courses have been added yet, creates a course collection for the user
-	// submit: adds course documents with a name, color, and date to the course collection
-<<<<<<< HEAD
-	document
-		.getElementById('courseForm')
-		.addEventListener('submit', function(e) {
-			console.log(courseName.value);
-			firebase.auth().onAuthStateChanged(function(user) {
-				db
-					.collection('users')
-					.doc(user.uid)
-					.collection('courses')
-					.doc()
-					.set(
-						{
-							course : {
-								name  : courseName.value,
-								color : courseColor.value,
-								date  : courseDate
+		// for a <form> with an id = 'courseForm'
+		// if no courses have been added yet, creates a course collection for the user
+		// submit: adds course documents with a name, color, and date to the course collection
+		document
+			.getElementById('courseForm')
+			.addEventListener('submit', function(e) {
+				console.log(courseName.value);
+				window.location.href = 'course-home.html';
+				firebase.auth().onAuthStateChanged(function(user) {
+					db
+						.collection('users')
+						.doc(user.uid)
+						.collection('courses')
+						.doc()
+						.set(
+							{
+								course : {
+									name  : courseName.value,
+									color : courseColor.value,
+									date  : courseDate
+								}
+							},
+							{
+								merge : true
 							}
-						},
-						{
-							merge : true
-						}
-					);
+						);
+				});
 			});
-		});
-}
-
-/** Displays Courses **/
-displayCourses();
-
-function displayCourses() {
-	firebase.auth().onAuthStateChanged(function(user) {
-		var dbRef = db.collection('users').doc(user.uid).collection('courses');
-		dbRef.onSnapshot(function(snap) {
-			//console.log(snap);
-			snap.forEach(function(doc) {
-				// retrieves color (hex) value from doc "course"
-				var colorPicked = doc.data().course.color;
-
-				// retrieves name value from doc "course"
-				var courseName = doc.data().course.name;
-
-				// creates a container that holds the course data
-				var courseContainer = document.createElement('div');
-				// assigns the container background with the color picked by user
-				courseContainer.style.backgroundColor = colorPicked;
-
-				// appends container to empty div
-				document
-					.getElementById('course_list')
-					.appendChild(courseContainer);
-
-				// this div will hold the course name + any other course info
-				var courseData = document.createElement('div');
-				courseContainer.appendChild(courseData);
-
-				// text node that displays the course name
-				var node = document.createTextNode(courseName);
-				courseData.appendChild(node);
-			});
-		});
-	});
-}
-=======
-	document.getElementById('courseForm').addEventListener('submit', function (e) {
-		
-		console.log(courseName.value);
-	   window.location.href = "course-home.html";
-	   firebase.auth().onAuthStateChanged(function (user) {
-		  db.collection("users").doc(user.uid).collection("courses").doc().set({
-			 "course": {
-				"name": courseName.value,
-				"color": courseColor.value,
-				"date": courseDate
-			 }
-		  }, {
-			 merge: true
-		  });
-
-	   });
-
-
-	});
- }
-
-}
- /** Displays Courses **/
-
-export const readDB = {
-	displayCourses: function () {
-		firebase.auth().onAuthStateChanged(function (user) {
-
-			var dbRef = db.collection("users").doc(user.uid).collection("courses");
-			dbRef.get().then(function (querySnapshot) {
-			   
-			 console.log("hi");
-	 
-			   querySnapshot.forEach(function (doc) {
-	 
-				 // retrieves color (hex) value from doc "course" 
-				  var colorPicked = doc.data().course.color;
-	 
-				 // retrieves name value from doc "course"
-				  var courseName = doc.data().course.name
-	 
-				 // creates a container that holds the course data
-				  var courseContainer = document.createElement("div");
-				 // assigns the container background with the color picked by user
-				  courseContainer.style.backgroundColor = colorPicked;
-				  courseContainer.setAttribute("class","courseContainer");
-				 
-				 // appends container to empty div
-				  document.getElementById("course_list").appendChild(courseContainer);
-				  
-				 // this div will hold the course name + any other course info
-				  var courseData = document.createElement("div");
-				  courseContainer.appendChild(courseData);
-				  courseData.setAttribute("class","courseCard");
-	 
-				 // text node that displays the course name
-				  var nameContainer = document.createElement("div");
-				  nameContainer.setAttribute("class","courseData");
-				  var courseName = document.createTextNode(courseName);
-				  var moreOptions = document.createElement("div");
-				 //  moreOptions.setAttribute("class","material-icons");
-				  moreOptions.innerHTML = "<i class='material-icons'>mdi_more_vert</i>";
-				  
-				  courseData.appendChild(nameContainer);
-				  nameContainer.appendChild(courseName);
-				  courseData.appendChild(moreOptions);
-	 
-			   });
-			});
-	 
-		 });
-	  
 	}
 };
+/** Displays Courses **/
 
->>>>>>> 689c9d59ca222d191f724549b93dc6cba5c28f47
+export const readDB = {
+	displayCourses : function() {
+		firebase.auth().onAuthStateChanged(function(user) {
+			var dbRef = db
+				.collection('users')
+				.doc(user.uid)
+				.collection('courses');
+			dbRef.get().then(function(querySnapshot) {
+				console.log('hi');
+
+				querySnapshot.forEach(function(doc) {
+					// retrieves color (hex) value from doc "course"
+					var colorPicked = doc.data().course.color;
+
+					// retrieves name value from doc "course"
+					var courseName = doc.data().course.name;
+
+					// creates a container that holds the course data
+					var courseContainer = document.createElement('div');
+					// assigns the container background with the color picked by user
+					courseContainer.style.backgroundColor = colorPicked;
+					courseContainer.setAttribute('class', 'courseContainer');
+
+					// appends container to empty div
+					document
+						.getElementById('course_list')
+						.appendChild(courseContainer);
+
+					// this div will hold the course name + any other course info
+					var courseData = document.createElement('div');
+					courseContainer.appendChild(courseData);
+					courseData.setAttribute('class', 'courseCard');
+
+					// text node that displays the course name
+					var nameContainer = document.createElement('div');
+					nameContainer.setAttribute('class', 'courseData');
+					var courseName = document.createTextNode(courseName);
+					var moreOptions = document.createElement('div');
+					//  moreOptions.setAttribute("class","material-icons");
+					moreOptions.innerHTML =
+						"<i class='material-icons'>mdi_more_vert</i>";
+
+					courseData.appendChild(nameContainer);
+					nameContainer.appendChild(courseName);
+					courseData.appendChild(moreOptions);
+				});
+			});
+		});
+	}
+};
 
 /*** Firestore Database ENDS ***/
