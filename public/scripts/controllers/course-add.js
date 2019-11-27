22eabf6640ba910f05b528fd78de933269331b74
Anthony Caron <antcar1992@gmail.com>
2019-11-26 16:47:41 -0800

    html forms cleaned up

--- public/scripts/controllers/course-add.js
+++ public/scripts/controllers/course-add.js
@@ -1,12 +1,14 @@
 import { courseAdd } from '../firebase/firebase.js';
 
+// Course Add Form
+const courseAddForm = document.getElementById('courseAddForm');
 // for an <input type='text'> with id = 'courseName'
 const courseName = document.getElementById('courseName');
 // for an <input type='color'> with id = 'courseColor'
 const courseColor = document.getElementById('courseColor');
 
 // for a <form> with an id = 'courseForm'
-document.getElementById('courseForm').addEventListener('submit', e => {
+courseAddForm.addEventListener('submit', e => {
 	e.preventDefault();
 	// Makes course object from form object
 	const course = {
