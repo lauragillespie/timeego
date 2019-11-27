7e058c525dd921e7551c934969b2421e0445931c
Cyra <cyraa@live.ca>
2019-11-26 22:31:53 -0800

    more deletion alert stuff

--- public/scripts/controllers/course-edit.js
+++ public/scripts/controllers/course-edit.js
@@ -1,7 +1,11 @@
 import { courseEdit } from '../models/firebase.js';
 
 const deleteButton = document.getElementById('delete');
-    
+
+const alert = document.getElementById('alertbox');  
+const noButton = document.getElementById('no_button');
+const yesButton = document.getElementById('yes_button');
+
 const urlString = (window.location.href);
 const url = new URL(urlString);
 const courseID = url.searchParams.get("courseid");
@@ -9,6 +13,22 @@ const courseID = url.searchParams.get("courseid");
 deleteButton.addEventListener( 'click', (e) => {
     e.preventDefault();
     
-    courseEdit.deleteCourse(courseID);
-    
+    // Prompts an alert confirming the course deletion
+    alertbox.classList.add('alertbox_active');
+
+    // Closes alert
+    noButton.addEventListener('click', e=>{
+        e.preventDefault();
+      
+        alertbox.classList.remove('alertbox_active');
+    })
+
+    // Deletes Course and closes alert, and redirects to course home
+    yesButton.addEventListener('click', e=>{
+        e.preventDefault();
+        courseEdit.deleteCourse(courseID);
+        alertbox.classList.remove('alertbox_active');
+        window.location.href = './course-home.html';
+    })
+
 });
