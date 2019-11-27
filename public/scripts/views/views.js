d7754b56e3714d04099aab00fbf320b56068ce6f
Anthony Caron <antcar1992@gmail.com>
2019-11-27 11:18:05 -0800

    fixed infinite loop bug

--- public/scripts/views/views.js
+++ public/scripts/views/views.js
@@ -5,19 +5,30 @@ export const timerViews = {
 	// Params: Array of Courses
 	// Renders: Course list in timer pop up
 	renderCourseList : function(arr) {
+		// Gets Course list in timer
 		const courseList = document.querySelector('.course-list__list');
-		// Sorts courses by name using helper function
-		const sortedCourses = helpers.sortCoursesByName(arr);
-		// Makes empty string
-		let listItems = '';
-		// Loops through array of courses, makes a string with info for each course, then appends new string to listItems string
-		sortedCourses.forEach(item => {
-			listItems += `<li class="course-list__list-item list-group-item list-group-item-action" id="${item.id}">
-				${item.name}
-			</li>`;
-		});
-		// After the forEach loop has finished, listItems contains html for each course. listItems is then added to html
-		courseList.innerHTML = listItems;
+
+		const filteredCourses = helpers.filterCurrentCourses(arr);
+		const arrayLength = filteredCourses;
+
+		// If course array is empty, makes and appends message to course list
+		if (arrayLength <= 0) {
+			courseList.innerHTML = `<li class="list-group-item" style="border: none;">
+			No Courses Currently Active
+		</li>`;
+		} else {
+			// sorts course array alphabetically
+			const sortedCourses = helpers.sortCoursesByName(filteredCourses);
+			let listItems = '';
+			// Loops through array of courses, makes a string with info for each course, then appends new string to listItems string
+			sortedCourses.forEach(item => {
+				listItems += `<li class="course-list__list-item list-group-item list-group-item-action" id="${item.id}">
+					${item.name}
+				</li>`;
+			});
+			// After the forEach loop has finished, listItems contains html for each course. listItems is then added to html
+			courseList.innerHTML = listItems;
+		}
 	}
 };
 
@@ -230,14 +241,15 @@ const helpers = {
 			totalMinutes += minutes;
 			totalHours += hours;
 		});
+
 		// Balances seconds and minutes to max of 59
 		while (totalSeconds >= 60) {
 			totalMinutes++;
-			totalSeconds - 60;
+			totalSeconds -= 60;
 		}
 		while (totalMinutes >= 60) {
 			totalHours++;
-			totalMinutes - 60;
+			totalMinutes -= 60;
 		}
 		// Returns total time
 		return {
